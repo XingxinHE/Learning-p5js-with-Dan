5f94566e06d60bd24156d29c65efd3da6109424b
Xingxin Morris He <30434598+XingxinMorris@users.noreply.github.com>
2017-11-07 18:03:45 +0800

    Update sketch.js

--- HTML Practice/sketch.js
+++ HTML Practice/sketch.js
@@ -3,26 +3,42 @@
 
 var theCanvas;
 var h1;
+var theButton;
+var bgColor;
 
-function setup(){
+function setup() {
 
   //create the canvas
-  theCanvas=createCanvas(400,400);
+  theCanvas = createCanvas(400, 400);
 
-//create the header; use the function 'createElement'
-  h1 = createElement("h1","I love my darling Li Peidie.")
+  //create the header; use the function 'createElement'
+  h1 = createElement("h1", "I love my darling Li Peidie.")
 
-//change their location
-//one important thing!!  what happened in the 'function draw' , their location
-//is relative to the canvas
-//BUT!!! the location of canvas and the header are relative to the page! the web page
-  theCanvas.position(800,400);
-  h1.position(200,200);
+  //create the button
+  theButton = createButton("Click me and change the background color");
+
+  bgColor = 220;
+
+
+  theButton.mousePressed(changeCol);
+
+  //change their location
+  //one important thing!!  what happened in the 'function draw' , their location
+  //is relative to the canvas
+  //BUT!!! the location of canvas and the header are relative to the page! the web page
+  theCanvas.position(800, 400);
+  h1.position(200, 200);
 }
 
-function draw(){
-  background(100);
+//create the change color function
+function changeCol() {
+  bgColor = random(255);
+}
+
+function draw() {
+
+  background(bgColor);
   fill(20);
   noStroke();
-  ellipse(width/2,height/2,66,66);
+  ellipse(width / 2, height / 2, 66, 66);
 }
